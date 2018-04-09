409f0a0c9da2ead33760a274d395684775ae6de3
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-04-09 19:38:14 +0530

    emits only to parent

--- src/main/scala/com/github/ahnfelt/react4s/samples/treeeditor/TreeNodeComponent.scala
+++ src/main/scala/com/github/ahnfelt/react4s/samples/treeeditor/TreeNodeComponent.scala
@@ -3,30 +3,40 @@ package com.github.ahnfelt.react4s.samples.treeeditor
 import com.github.ahnfelt.react4s._
 import com.github.ahnfelt.react4s.samples.theme.LinkCss
 
-case class TreeNodeComponent(node : P[TreeNode]) extends Component[TreeEvent] {
+case class TreeNodeComponent(node: P[TreeNode]) extends Component[TreeEvent] {
 
-    override def render(get : Get) =
-        E.div(
-            E.input(A.onChangeText(t => emit(SetLabel(t))), A.value(get(node).label)),
-            renderButton("chevron-up", MoveUp),
-            renderButton("chevron-down", MoveDown),
-            renderButton("trash", Delete),
-            renderButton("plus-circle", SetChildren(get(node).children :+ TreeNode("", List()))),
-            renderTodoList(get(node).children),
-        )
-
-    def renderButton(icon : String, event : TreeEvent) = E.span(
-        S.paddingLeft.px(5),
-        E.i(A.className("fa fa-" + icon), LinkCss, A.onLeftClick(_ => emit(event)))
+  override def render(get: Get) =
+    E.div(
+      E.input(A.onChangeText(t => emit(SetLabel(t))), A.value(get(node).label)),
+      renderButton("chevron-up", MoveUp),
+      renderButton("chevron-down", MoveDown),
+      renderButton("trash", Delete),
+      renderButton("plus-circle",
+                   SetChildren(get(node).children :+ TreeNode("", List()))),
+      renderTodoList(get(node).children),
     )
 
-    def renderTodoList(nodes : List[TreeNode]) =
-        E.ul(Tags(
-            for((item, index) <- nodes.zipWithIndex) yield E.li(
-                Component(TreeNodeComponent, item).withHandler { e =>
-                    val newChildren = TreeEvent.update(nodes, e, index)
-                    emit(SetChildren(newChildren))
-                }
+  def renderButton(icon: String, event: TreeEvent) = E.span(
+    S.paddingLeft.px(5),
+    E.i(A.className("fa fa-" + icon), LinkCss, A.onLeftClick(_ => {
+      println(s"emmitting event ${event.toString}")
+      emit(event)
+    }))
+  )
+
+  def renderTodoList(nodes: List[TreeNode]) =
+    E.ul(
+      Tags(
+        for ((item, index) <- nodes.zipWithIndex)
+          yield
+            E.li(
+              Component(TreeNodeComponent, item).withHandler { e =>
+                println(
+                  "withhandler called for event in treenode " + e.toString)
+                val newChildren = TreeEvent.update(nodes, e, index)
+                println("emitting event set children")
+                emit(SetChildren(newChildren))
+              }
             )
-        ))
+      ))
 }
--- src/main/scala/com/github/ahnfelt/react4s/samples/treeeditor/TreeRootComponent.scala
+++ src/main/scala/com/github/ahnfelt/react4s/samples/treeeditor/TreeRootComponent.scala
@@ -2,18 +2,24 @@ package com.github.ahnfelt.react4s.samples.treeeditor
 
 import com.github.ahnfelt.react4s._
 
-case class TreeRootComponent(nodes : P[List[TreeNode]]) extends Component[NoEmit] {
+case class TreeRootComponent(nodes: P[List[TreeNode]])
+    extends Component[NoEmit] {
 
-    val children = State.of(nodes)
+  val children = State.of(nodes)
 
-    override def render(get : Get) = E.div(
-        E.h3(Text("Tree editor")),
-        E.ul(Tags(
-            for((item, index) <- get(children).zipWithIndex) yield E.li(
-                Component(TreeNodeComponent, item).withHandler { e =>
-                    children.modify(TreeEvent.update(_, e, index))
-                }
+  override def render(get: Get) = E.div(
+    E.h3(Text("Tree editor")),
+    E.ul(
+      Tags(
+        for ((item, index) <- get(children).zipWithIndex)
+          yield
+            E.li(
+              Component(TreeNodeComponent, item).withHandler { e =>
+                println(
+                  "withhandler called with event in tree root " + e.toString)
+                children.modify(TreeEvent.update(_, e, index))
+              }
             )
-        ))
-    )
+      ))
+  )
 }
