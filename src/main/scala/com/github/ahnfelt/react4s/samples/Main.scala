fcde3067e5d7da2da53954525e57e7da8eee9dc0
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-04-16 23:35:21 +0200

    Search engine optimization attempt

--- src/main/scala/com/github/ahnfelt/react4s/samples/Main.scala
+++ src/main/scala/com/github/ahnfelt/react4s/samples/Main.scala
@@ -1,10 +1,13 @@
 package com.github.ahnfelt.react4s.samples
 
 import com.github.ahnfelt.react4s._
+import org.scalajs.dom
 
 object Main {
     def main(arguments : Array[String]) : Unit = {
-        val component = Component(MainComponent)
-        ReactBridge.renderToDomById(component, "main")
+        dom.window.onload = _ => {
+            val component = Component(MainComponent)
+            ReactBridge.renderToDomById(component, "main")
+        }
     }
 }
